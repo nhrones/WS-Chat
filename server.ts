3cdd96b2a5878c36c37c24bf5c36d7353cdf68ff
Nick Hrones <nhrones@users.noreply.github.com>
2021-10-08 15:09:27 -0400

    name

--- server.ts
+++ server.ts
@@ -1,3 +1,13 @@
+const {
+    listen,
+    serveHttp,
+    readFile,
+    upgradeWebSocket,
+    env
+    //@ts-ignore ?
+} = Deno
+//@ts-ignore ?
+type Conn = Deno.Conn
 
 /** WebSocket Client */
 type Client = {
@@ -20,16 +30,16 @@ chatChannel.onmessage = (e: MessageEvent) => {
 const webSockets = new Map<string, Client>()
 
 /** Deploy Environment */
-const DEV: boolean = (Deno.env.get("DEV") === "true")
-const DEBUG = (Deno.env.get("DEBUG") === "true")
-const INTERVAL = parseInt(Deno.env.get("INTERVAL") || "30000")
-if (DEBUG) console.log(`Env DEV: ${DEV}, DEBUG: ${DEBUG} INTERVAL: ${INTERVAL} DEPLOYMENT_ID: ${Deno.env.get("DENO_DEPLOYMENT_ID")}`)
+const DEV: boolean = (env.get("DEV") === "true")
+const DEBUG = (env.get("DEBUG") === "true")
+const INTERVAL = parseInt(env.get("INTERVAL") || "30000")
+if (DEBUG) console.log(`Env DEV: ${DEV}, DEBUG: ${DEBUG} INTERVAL: ${INTERVAL} DEPLOYMENT_ID: ${env.get("DENO_DEPLOYMENT_ID")}`)
 
 /** load an index.html file (clients) */
 async function handleStaticFile() {
     try {
         const path = "./index.html"
-        const body = await Deno.readFile(path)
+        const body = await readFile(path)
         const headers = new Headers()
         headers.set("content-type", "text/html")
         return new Response(body, { status: 200, headers })
@@ -39,66 +49,77 @@ async function handleStaticFile() {
     }
 }
 
-const listener = Deno.listen({ port: 8080 });
+const listener = listen({ port: 8080 });
 console.log("listening on http://localhost:8080")
 
+//@ts-ignore
 for await (const conn of listener) {
     handleConnection(conn)
 }
 
 /** Handle each new connection */
-async function handleConnection(conn: Deno.Conn) {
-    const httpConn = Deno.serveHttp(conn);
-    for await (const { request, respondWith } of httpConn) {
-        if (request.headers.get("upgrade") === "websocket") {
-            const { socket, response } = Deno.upgradeWebSocket(request);
-            const client: Client = { id: '', name: '', isAlive: true, socket: socket }
-            socket.onopen = () => {
-                client.id = request.headers.get('sec-websocket-key') || ""
-                if (DEBUG) console.log("Client connected ... id: " + client.id)
-                // Register our new socket(user)
-                webSockets.set(client.id, client)
-            }
-            socket.onmessage = (msg) => {
-                const data = msg.data
-                if (typeof data === 'string') {
-                    // user registration request?
-                    if (data.startsWith('Register')) {
-                        // get the users name from the data string('Register:John Doe')
-                        client.name = data.split(":")[1]// the second value of split-array
-                        if (DEBUG) console.log(`${client.name} >> has joined the chat!`)
-                        broadcast(`${client.name} >> has joined the chat!`);
-                    } else if (data === 'ACK') { // watchdog acknowledged
-                        if (DEV) console.log(`Recieved watchdog 'ACK' from ${client.name}`)
-                        client.isAlive = true
-                    } else {
-                        if (DEBUG) console.log(`${client.name} >> ${msg.data}`)
-                        broadcast(`${client.name} >> ${msg.data}`)
-                    }
-                }
-            }
-            socket.onclose = () => {
-                const name = webSockets.get(client.id)?.name || 'someone'
-                webSockets.delete(client.id);
-                broadcast(`${name} has disconnected`)
-                if (DEBUG) console.log(name + " disconnected from chat ...")
-            }
-
-            socket.onerror = (err: Event | ErrorEvent) => {
-                console.log(err instanceof ErrorEvent ? err.message : err.type)
-            }
-
-            respondWith(response);
-
-            // watchdog timer
-            setInterval(() => watchDog(), INTERVAL)
-
-
-        } else { // not a webSocket request just load our html
-            respondWith(await handleStaticFile())
-        }
+async function handleConnection(conn: Conn) {
+    const httpConn = serveHttp(conn);
+    //for await (const { request, respondWith } of httpConn) {
+    for await (const requestEvent of httpConn) {
+        await requestEvent.respondWith(handleRequest(requestEvent.request));
     }
 }
+
+/** handle each new http request */
+async function handleRequest(request: Request): Promise<Response> {
+
+    if (request.headers.get("upgrade") === "websocket") {
+        const { socket, response } = upgradeWebSocket(request);
+        const client: Client = { id: '', name: '', isAlive: true, socket: socket }
+        socket.onopen = () => {
+            client.id = request.headers.get('sec-websocket-key') || ""
+            if (DEBUG) console.log("Client connected ... id: " + client.id)
+            // Register our new socket(user)
+            webSockets.set(client.id, client)
+        }
+        socket.onmessage = (msg) => {
+            const data = msg.data
+            if (typeof data === 'string') {
+                // user registration request?
+                if (data.startsWith('Register')) {
+                    // get the users name from the data string('Register:John Doe')
+                    client.name = data.split(":")[1]// the second value of split-array
+                    if (DEBUG) console.log(`${client.name} >> has joined the chat!`)
+                    broadcast(`${client.name} >> has joined the chat!`);
+                } else if (data === 'ACK') { // watchdog acknowledged
+                    if (DEV) console.log(`Recieved watchdog 'ACK' from ${client.name}`)
+                    client.isAlive = true
+                } else {
+                    if (DEBUG) console.log(`${client.name} >> ${msg.data}`)
+                    broadcast(`${client.name} >> ${msg.data}`)
+                }
+            }
+        }
+        socket.onclose = () => {
+            const name = webSockets.get(client.id)?.name || 'someone'
+            webSockets.delete(client.id);
+            broadcast(`${name} has disconnected`)
+            if (DEBUG) console.log(name + " disconnected from chat ...")
+        }
+
+        socket.onerror = (err: Event | ErrorEvent) => {
+            console.log(err instanceof ErrorEvent ? err.message : err.type)
+        }
+
+        return response
+        //respondWith(response);
+
+        // watchdog timer
+        setInterval(() => watchDog(), INTERVAL)
+
+
+    } else { // not a webSocket request just load our html
+        //respondWith(await handleStaticFile())
+        return await handleStaticFile()
+    }
+}
+
 
 /** broadcasts a message to every registered user on all isolates */
 function broadcast(msg: string): void {
